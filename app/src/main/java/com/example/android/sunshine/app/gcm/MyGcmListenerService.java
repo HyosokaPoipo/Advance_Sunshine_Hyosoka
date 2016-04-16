400c2acadf6180097a338396a8d6c32cf32205be
Maryadi <maryadi@elephanttalk.com>
2016-04-16 15:06:33 +0700

    FirstService

---
+++ app/src/main/java/com/example/android/sunshine/app/gcm/MyGcmListenerService.java
@@ -0,0 +1,7 @@
+package com.example.android.sunshine.app.gcm;
+
+/**
+ * Created by Etit-Ind8 on 4/15/2016.
+ */
+public class MyGcmListenerService {
+}
---
+++ app/src/main/java/com/example/android/sunshine/app/gcm/MyInstanceIDListenerService.java
@@ -0,0 +1,17 @@
+package com.example.android.sunshine.app.gcm;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by HyosokaPoipo on 4/15/2016.
+ */
+public class MyInstanceIDListenerService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
